2d5b4b57eab5ba0c67f8a0e7da2b0ebe0e5e4933
381706-1VlasovAndrey <andrio.vlasov.2000@mail.ru>
2020-03-21 14:25:56 +0300

    fix

--- modules/task_2/tihomirova_m_integration_monte_carlo/integration_monte_carlo.h
+++ modules/task_2/tihomirova_m_integration_monte_carlo/integration_monte_carlo.h
@@ -2,9 +2,9 @@
 #ifndef MODULES_TASK_2_TIHOMIROVA_M_INTEGRATION_MONTE_CARLO_INTEGRATION_MONTE_CARLO_H_
 #define MODULES_TASK_2_TIHOMIROVA_M_INTEGRATION_MONTE_CARLO_INTEGRATION_MONTE_CARLO_H_
 
+#include <omp.h>
 #include <vector>
 #include <functional>
-#include <omp.h>
 
 double getIntegralOMP(int n, const std::vector<double>& low, const std::vector<double>& top,
   const std::function<double(const std::vector<double>&)>& f);
